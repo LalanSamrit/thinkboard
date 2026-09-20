cb3402a3db4fba209f5a4aa890f4ecb8c1b871fb
Lalan samrit <lalansamrit3@gmail.com>
2026-09-21 00:15:59 +0530

    initial commit

---
+++ backend/src/middleware/rateLimiter.js
@@ -0,0 +1,19 @@
+import ratelimit from "../config/upstash.js";
+
+const rateLimiter = async (req, res, next) => {
+  try {
+    const { success } = await ratelimit.limit("my-limit-key");
+
+    if (!success) {
+      return res
+        .status(429)
+        .json({ message: "To many requests, pleas try again later" });
+    }
+    next();
+  } catch (error) {
+    console.log("Rate limit error", error);
+    next(error);
+  }
+};
+
+export default rateLimiter;
